020312111037a7078dde115da0245028ce775a74
Lucas Queiroz <lucascqueiroz97@gmail.com>
2020-12-06 02:51:30 -0300

    Day 4 - Part 2

--- day-4/part-2/src/main.rs
+++ day-4/part-2/src/main.rs
@@ -14,108 +14,45 @@ fn main() -> io::Result<()> {
         if !(line_str.eq("")) {
             current.push_str(&line_str);
         } else {
-            if
-                current.contains("byr:") &&
-                current.contains("iyr:") &&
-                current.contains("eyr:") &&
-                current.contains("hgt:") &&
-                current.contains("hcl:") &&
-                current.contains("ecl:") &&
-                current.contains("pid:")
-            {
-                let bcurrent = &current.clone();
-
-                let mut re = Regex::new(r"byr:(\d{4})").unwrap();
-                if !re.is_match(bcurrent) {
-                    continue;
-                }
-                let byr = re.captures(bcurrent).unwrap().get(1).unwrap().as_str();
-
-                re = Regex::new(r"iyr:(\d{4})").unwrap();
-                if !re.is_match(bcurrent) {
-                    continue;
-                }
-                let iyr = re.captures(bcurrent).unwrap().get(1).unwrap().as_str();
-
-                re = Regex::new(r"eyr:(\d{4})").unwrap();
-                if !re.is_match(bcurrent) {
-                    continue;
-                }
-                let eyr = re.captures(bcurrent).unwrap().get(1).unwrap().as_str();
-
-                re = Regex::new(r"hgt:(\d{2,3}[ic][nm])").unwrap();
-                if !re.is_match(bcurrent) {
-                    continue;
-                }
-                let hgt = re.captures(bcurrent).unwrap().get(1).unwrap().as_str();
-
-                re = Regex::new(r"hcl:(#[0-9a-f]{6})").unwrap();
-                if !re.is_match(bcurrent) {
-                    continue;
-                }
-                let hcl = re.captures(bcurrent).unwrap().get(1).unwrap().as_str();
-
-                re = Regex::new(r"ecl:(gry|amb|blu|brn|grn|hzl|oth)").unwrap();
-                if !re.is_match(bcurrent) {
-                    continue;
-                }
-                let ecl = re.captures(bcurrent).unwrap().get(1).unwrap().as_str();
-
-                re = Regex::new(r"pid:(\d{9})").unwrap();
-                if !re.is_match(bcurrent) {
-                    continue;
-                }
-                let pid = re.captures(bcurrent).unwrap().get(1).unwrap().as_str();
-
-                current = "".to_string();
-
-                if
-                    byr.is_empty() ||
-                    iyr.is_empty() ||
-                    eyr.is_empty() ||
-                    hgt.is_empty() ||
-                    hcl.is_empty() ||
-                    ecl.is_empty() ||
-                    pid.is_empty()
-                {
-                    continue;
-                }
-
-                let byr_val: u32 = byr.parse().expect("wanted a number");
-
-                if byr_val < 1920 || byr_val > 2020 {
-                    continue;
-                }
-
-                let iyr_val: u32 = iyr.parse().expect("wanted a number");
-
-                if iyr_val < 2010 || iyr_val > 2020 {
-                    continue;
-                }
-
-                let eyr_val: u32 = eyr.parse().expect("wanted a number");
-
-                if eyr_val < 2020 || eyr_val > 2030 {
-                    continue;
-                }
-
-                if hgt.contains("cm") {
-                    let hgt_val: u32 = hgt.replace("cm", "").parse().expect("wanted a number");
-
-                    if hgt_val < 150 || hgt_val > 193 {
-                        continue;
-                    }
-                } else {
-                    let hgt_val: u32 = hgt.replace("in", "").parse().expect("wanted a number");
-
-                    if hgt_val < 59 || hgt_val > 76 {
-                        continue;
-                    }
-                }
-
-                valid = valid + 1;
-            }
+            let bcurrent = &current.clone();
             current = "".to_string();
+
+            let mut re = Regex::new(r"byr:(?:200[0-2]|19[2-9][0-9])").unwrap();
+            if !re.is_match(bcurrent) {
+                continue;
+            }
+
+            re = Regex::new(r"iyr:20(?:1[0-9]|20)").unwrap();
+            if !re.is_match(bcurrent) {
+                continue;
+            }
+
+            re = Regex::new(r"eyr:20(?:2[0-9]|30)").unwrap();
+            if !re.is_match(bcurrent) {
+                continue;
+            }
+
+            re = Regex::new(r"hgt:(?:1(?:[5-8][0-9]|9[0-3])cm|(?:59|6[0-9]|7[0-6])in)").unwrap();
+            if !re.is_match(bcurrent) {
+                continue;
+            }
+
+            re = Regex::new(r"hcl:#[0-9a-f]{6}[\D^a-f\n]?").unwrap();
+            if !re.is_match(bcurrent) {
+                continue;
+            }
+
+            re = Regex::new(r"ecl:(?:amb|blu|brn|gry|grn|hzl|oth)[\D\n]?").unwrap();
+            if !re.is_match(bcurrent) {
+                continue;
+            }
+
+            re = Regex::new(r"pid:(?:\d{9})(?:$|\n|\D|[bieh])").unwrap();
+            if !re.is_match(bcurrent) {
+                continue;
+            }
+
+            valid = valid + 1;
         }
     }
     println!("{}", valid);
