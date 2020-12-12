f3b8b56ac943d157944c00a867f3215fae4c72ae
Lucas Queiroz <lucascqueiroz97@gmail.com>
2020-12-12 14:10:30 -0300

    Day 6 - Part 1

---
+++ day-6/part-1.py
@@ -0,0 +1,25 @@
+import os
+import re
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, 'input')
+
+input = open(filename, "r")
+
+current = ""
+
+lines = input.readlines()
+last = len(lines) - 1
+total = 0
+i = 0
+
+for line in lines:
+    if line == "\n" or i == last:
+        print(current)
+        total += len(set(current.strip()))
+        current = ""
+    else:
+        current += line.strip()
+
+    i += 1
+
+print(total)
