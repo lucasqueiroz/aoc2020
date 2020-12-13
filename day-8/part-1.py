b9928eceaa845fdab2410bb60788e67e771f26f1
Lucas Queiroz <lucascqueiroz97@gmail.com>
2020-12-13 11:01:09 -0300

    Day 8 - Part 1

---
+++ day-8/part-1.py
@@ -0,0 +1,37 @@
+import os
+dirname = os.path.dirname(__file__)
+filename = os.path.join(dirname, 'input')
+
+input = open(filename, "r")
+
+lines = input.readlines()
+
+instructions = {}
+acc = 0
+i = 0
+
+while i < len(lines) - 1:
+    line = lines[i].strip()
+
+    if i in instructions.get(line, []):
+        break
+
+    if instructions.get(line):
+        instructions[line].append(i)
+    else:
+        instructions[line] = [i]
+
+    if line.startswith("nop"):
+        i += 1
+        continue
+
+    if line.startswith("acc"):
+        line = line.replace("acc ", "")
+        acc += int(line.strip())
+        i += 1
+
+    if line.startswith("jmp"):
+        line = line.replace("jmp ", "")
+        i += int(line.strip())
+
+print(acc)
